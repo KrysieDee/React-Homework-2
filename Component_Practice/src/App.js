98dd9059a41e69205a8a4475852f5d9e24ea11fe
KrysieDee <krystal.smithx@gmail.com>
2019-01-04 17:10:49 -0500

    homework

--- Component_Practice/src/App.js
+++ Component_Practice/src/App.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar.js'
+import Home from './components/Home.js'
+import Main from './components/Main.js'
+import Projects from './components/Projects.js'
+import Resume from './components/Resume.js'
+import Contact from './components/Contact.js'
+import SocialLinks from './components/SocialLinks.js'
+import Footer from './components/Footer.js'
+
 
 import './App.css';
 
--- Component_Practice/src/components/Projects.js
+++ Component_Practice/src/components/Projects.js
@@ -1,1 +1,28 @@
+import React, { Component } from 'react'; 
 import projects from '../data/projects.json';
+import Project from './Project.js';
+
+class Projects extends Component {
+    constructor(imgName, title, description){
+        super(),
+        this.state = {
+           done: projects.allProjects 
+        }
+    }
+    render(){
+        return(
+            <div>{
+              this.state.done.map((k)=>{
+               return <Project 
+               imgName = {k.imgName}
+               title = {k.title}
+               description = {k.description}
+               />
+           })}
+           </div>
+        )
+    
+    }
+}
+
+export default Projects; 
